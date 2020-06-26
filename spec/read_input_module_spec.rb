2ea074cddb9d60d8cb5fdab92ee14f61c465dca5
Nicolas Hermet <n.hermet@gmail.com>
2020-06-26 14:25:11 +0200

    test(read input): check if cars and rentals are extracted from input

--- spec/read_input_module_spec.rb
+++ spec/read_input_module_spec.rb
@@ -16,3 +16,41 @@ describe 'Read file method' do
         expect(method).to have_key(:rentals)
     end
 end
+
+describe 'Get cars Array from input' do
+    get_cars = ReadInput.get_cars_from_input
+    it 'should not raise any error' do
+        expect{get_cars}.not_to raise_error
+    end
+
+    it 'should return an Array' do
+        expect(get_cars.class).to eq(Array)
+    end
+
+    it 'should have 3 elements' do
+        expect(get_cars.length).to eq(3)
+    end
+
+    it 'should be the correct data' do
+        expect(get_cars[0]).to eq({ "id": 1, "price_per_day": 2000, "price_per_km": 10 })
+    end
+end
+
+describe 'Get rentals Array from input' do
+    get_rentals = ReadInput.get_rentals_from_input
+    it 'should not raise any error' do
+        expect{get_rentals}.not_to raise_error
+    end
+
+    it 'should return an Array' do
+        expect(get_rentals.class).to eq(Array)
+    end
+
+    it 'should have 3 elements' do
+        expect(get_rentals.length).to eq(3)
+    end
+
+    it 'should be the correct data' do
+        expect(get_rentals[0]).to eq({ "id": 1, "car_id": 1, "start_date": "2017-12-8", "end_date": "2017-12-10", "distance": 100 })
+    end
+end
