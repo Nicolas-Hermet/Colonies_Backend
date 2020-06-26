4c63ba660f99c657c3d98341aab367fd3adbaac4
Nicolas Hermet <n.hermet@gmail.com>
2020-06-26 14:26:01 +0200

    feat(read input): extract cars and rentals arrays from input

--- level1/lib/read_input_module.rb
+++ level1/lib/read_input_module.rb
@@ -7,4 +7,12 @@ module ReadInput
         input = File.read($LOAD_PATH[0] + '/input.json')
         JSON.parse(input, {symbolize_names: true})
     end
+
+    def self.get_cars_from_input
+        read_file[:cars]
+    end
+
+    def self.get_rentals_from_input
+        read_file[:rentals]
+    end
 end
