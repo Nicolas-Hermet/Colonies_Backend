91b7b34b9cfa1f1763454783ca62c2753d88f238
Nicolas Hermet <n.hermet@gmail.com>
2020-06-27 13:27:21 +0200

    test(rental class): check that price is an integer

--- spec/rental_spec.rb
+++ spec/rental_spec.rb
@@ -21,5 +21,6 @@ describe 'Rental objects' do
 
     it "can compute price" do
         expect(rental.send(:get_price)).to eq(6600) # 3 days for (2000 + 2*2000-10%) + 100km for 10 each
+        expect(rental.send(:get_price).class).to be(Integer)
     end
 end
