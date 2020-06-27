d9f85bc6d908225166b000e64455b597e8d88c82
Nicolas Hermet <n.hermet@gmail.com>
2020-06-27 13:27:46 +0200

    feat(rental class): round price to closest integer

--- level2/lib/rental.rb
+++ level2/lib/rental.rb
@@ -19,14 +19,15 @@ class Rental
         price = 0
         duration = (@end_date - @start_date).to_i + 1
         car_price = @car.price_per_day
-        
+
         price = case duration
             when 0..1 then car_price * duration
-            when 2..4 then car_price * duration + (duration - 1) * car_price * 0.9
-            when 5..10 then car_price * duration + 3 * car_price * 0.9 + (duration - 4) * car_price * 0.7
-            when 10..Float::INFINITY then car_price * duration + 3 * car_price * 0.9 + 6 * car_price * 0.7 + (duration - 10) * car_price * 0.5
+            when 2..4 then car_price + (duration - 1) * car_price * 0.9
+            when 5..10 then car_price + 3 * car_price * 0.9 + (duration - 4) * car_price * 0.7
+            when 11..Float::INFINITY then car_price + 3 * car_price * 0.9 + 6 * car_price * 0.7 + (duration - 10) * car_price * 0.5
             else "Invalid Dates"
         end
         price += @car.price_per_km * @distance
+        price.round
     end
 end
