199f69cbd045cc101ffca50888fbebecc8106b8a
Nicolas Hermet <n.hermet@gmail.com>
2020-06-27 07:30:09 +0200

    feat(main): get a car from its id

--- level1/main.rb
+++ level1/main.rb
@@ -16,3 +16,7 @@ def create_rentals_array_from_input
         $rentals.push(Rental.new(rental[:id], car_from_its_id(rental[:car_id]), rental[:start_date], rental[:end_date], rental[:distance]))
     end
 end
+
+def car_from_its_id car_id
+    $cars[$cars.index{|c| c.id === car_id}]
+end
