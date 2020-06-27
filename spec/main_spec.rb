3a45115a87146678fe70c5aa58b8fa9306e024d8
Nicolas Hermet <n.hermet@gmail.com>
2020-06-27 07:27:22 +0200

    test(main): check that one can get a Car from cars ...
    ...thanks to the car.id.

--- spec/main_spec.rb
+++ spec/main_spec.rb
@@ -30,3 +30,17 @@ describe 'Main create_rentals_array_from_input' do
         expect($rentals[random_index]).to be_instance_of(Rental)
     end
 end
+
+describe 'Main car_from_its_id' do
+    it 'get a car from $cars thanks to an id' do
+        expect(car_from_its_id 1).to be_instance_of(Car)
+    end
+
+    it 'get the right car from $cars' do
+        actual_car = car_from_its_id(1)
+        should_be_car = Car.new(1, 2000, 10)
+        expect(actual_car.id).to be(should_be_car.id)
+        expect(actual_car.price_per_day).to be(should_be_car.price_per_day)
+        expect(actual_car.price_per_km).to be(should_be_car.price_per_km)
+    end
+end
