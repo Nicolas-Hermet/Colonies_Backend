87c7d792ec2332220e08a435952353d7c6155be1
Nicolas Hermet <n.hermet@gmail.com>
2020-06-27 14:26:21 +0200

    refactor(rental class): dry the duration value
    this will allow to reuse the duration computation

--- level3/lib/rental.rb
+++ level3/lib/rental.rb
@@ -8,6 +8,7 @@ class Rental
         @car = car
         @start_date = Date.parse(start_date)
         @end_date = Date.parse(end_date)
+        @duration = (@end_date - @start_date).to_i + 1
         @distance = distance
 
         @price = set_price
@@ -17,14 +18,13 @@ class Rental
 
     def set_price
         price = 0
-        duration = (@end_date - @start_date).to_i + 1
         car_price = @car.price_per_day
 
-        price = case duration
-            when 0..1 then car_price * duration
-            when 2..4 then car_price + (duration - 1) * car_price * 0.9
-            when 5..10 then car_price + 3 * car_price * 0.9 + (duration - 4) * car_price * 0.7
-            when 11..Float::INFINITY then car_price + 3 * car_price * 0.9 + 6 * car_price * 0.7 + (duration - 10) * car_price * 0.5
+        price = case @duration
+            when 0..1 then car_price * @duration
+            when 2..4 then car_price + (@duration - 1) * car_price * 0.9
+            when 5..10 then car_price + 3 * car_price * 0.9 + (@duration - 4) * car_price * 0.7
+            when 11..Float::INFINITY then car_price + 3 * car_price * 0.9 + 6 * car_price * 0.7 + (@duration - 10) * car_price * 0.5
             else "Invalid Dates"
         end
         price += @car.price_per_km * @distance
