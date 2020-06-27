c8d769174fe400f3fc7a4100abe903d206584f15
Nicolas Hermet <n.hermet@gmail.com>
2020-06-27 08:15:05 +0200

    feat(main): write json output file

--- level1/main.rb
+++ level1/main.rb
@@ -3,6 +3,7 @@ require 'read_input_module'
 require 'Car'
 require 'Rental'
 
+$MAIN_ROOT = File.dirname __FILE__
 $cars = []
 $rentals = []
 def create_cars_array_from_input
@@ -24,5 +25,8 @@ end
 def write_output
     output = {rentals: []}
     $rentals.each{|r| output[:rentals].push({id: r.id, price: r.price})}
+    File.open("#{$MAIN_ROOT}/data/actual_output.json",'w') do |f|
+        f.write(output.to_json)
+    end
     output
 end
